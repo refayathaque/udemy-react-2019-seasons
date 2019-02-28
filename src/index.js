0483504946dd04cad6ca06710e1b85231edfed11
Refayat Haque <refayathaque@gmail.com>
2019-02-28 12:21:24 -0500

    staged, committed and pushing to master

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ import ReactDOM from 'react-dom';
 //   );
 // };
 
-// Updating 'state' on a component causes the component to (almost) instantly rerender
-
 // Class-Based Component
 class App extends Component {
   // Any time an instance of the class is created the constructor function is the first function that will be called
@@ -24,16 +22,21 @@ class App extends Component {
     // State must be initialized when a component is created, and that can be done here in the constructor function
     super(props); // Ensures that the React.Component's 'constructor' function, with all of it's necessary configuration, gets called
     // ^ Necessary, or else, you will get an error
-    // This (below) is the only time we do a direct assignment to `this.state`
+    // This (below) is the only time we do a direct assignment to `this.state`, we are initializing state here
     this.state = {
       lat: null,
-      lon: null
+      long: null
     };
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // State can only be updated using the function 'setState'
         // And we NEVER want to do a direct assignment to a state object, e.g., `this.state.lat = position.coords.latitude`, unless we are initializing the state inside the constructor function
-        this.setState({ lat: position.coords.latitude })
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+        console.log(position)
+        // Updating 'state' on a component causes the component to (almost) instantly rerender the component
       },
       (err) => console.log(err)
     );
@@ -41,7 +44,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        Latitude: {this.state.lat}
+        <p>Latitude: {this.state.lat}</p>
+        <p>Longtitude: {this.state.long}</p>
       </div>
     );
   };
