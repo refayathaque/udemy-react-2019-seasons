52207f94002d310bb08ca0d447f3384c8b4552d2
Refayat Haque <refayathaque@gmail.com>
2019-02-28 10:15:56 -0500

    staged, committed and pushing to master

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+// ^ ES6 Destructuring
 import ReactDOM from 'react-dom';
 
-const App = () => {
+// Functional Component
+// const App = () => {
+//
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//
+//   return (
+//     <div>
+//       Hi there!
+//     </div>
+//   );
+// };
 
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+// Class-Based Component
+class App extends Component {
+  render () {
 
-  return (
-    <div>
-      Hi there!
-    </div>
-  );
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return (
+      <div>
+        Hi there!
+      </div>
+    );
+  };
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
