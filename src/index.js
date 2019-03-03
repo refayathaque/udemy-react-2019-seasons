37db3d7608152220ffb38077770329b603f963cd
Refayat Haque <refayathaque@gmail.com>
2019-03-03 08:47:59 -0500

    staged, committed and pushing to master

--- src/index.js
+++ src/index.js
@@ -23,14 +23,14 @@ import ReactDOM from 'react-dom';
 //   </div>
 // )
 
-// Component Lifecycle functions are functions we can optionally (render is not optional) define in our class-based components
+// Component lifecycle functions are functions we can optionally (render is not optional) define in our class-based components
 // constructor, render, componentDidMount, componentDidUpdate, componentWillUnmount
 
 // Class-Based Component
 class App extends Component {
   // Any time an instance of the class is created the constructor function is the first function that will be automatically called
   constructor(props) {
-    // State must be initialized when a component is created, and that can be done here in the constructor function
+    // State must be initialized when a component is created, and that can be done here in the lifecycle constructor function
     super(props); // Ensures that the React.Component's 'constructor' function, with all of it's necessary configuration, gets called
     // ^ Necessary, or else, you will get an error
     // This (below) is the only time we do a direct assignment to `this.state`, since we are initializing state here
@@ -61,11 +61,14 @@ class App extends Component {
   }
   componentDidMount() {
     // This lifecycle function will be automatically called once, when our component first gets rendered on to the screen
+    // Perfect place to do some initial data loading! E.g., API calls to fetch data
+    // React Community Best-Practice and Official React Documentation advise against data loading in the constructor lifecycle function, they suggest that initial data loading happen here in the componentDidMount lifecycle method. This is recommended to ensure more clear code, e.g., a new developer will know that all API calls/data loading code are in this lifecycle function and won't have to look elsewhere
     console.log('My component was rendered to the screen');
   }
   componentDidUpdate() {
     // This lifecycle function will be automatically called when the component updates itself and re-renders because we called `setState`
     // Will get called every time the component gets updated
+    // Good place to do more data loading when state changes, or our component gets updated props from its parent. E.g., we want to execute an API call every single time a user clicks on a button, or enters text to an input
     console.log('My component was just updated - it re-rendered!');
   }
   render() {
